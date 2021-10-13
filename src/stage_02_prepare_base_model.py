4c383e120f181a2cc5890ba3ca4128933c47d969
jitendra <jitendra_shakya@hotmail.com>
2021-10-13 13:38:33 +0100

    stage 02 second update

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,17 +1,15 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
-import pandas as pd
 import os
-import shutil
-from tqdm import tqdm
 import logging
+import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir, "running_logs.log"), level=logging.INFO, format=logging_str, filemode="a")
-
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
 
 def prepare_base_model(config_path, params_path):
     config = read_yaml(config_path)
@@ -29,8 +27,31 @@ def prepare_base_model(config_path, params_path):
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
-    model = get_VGG_16_model(input_shape= params["IMAGE_SIZE"], model_path= base_model_path)
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path=base_model_path)
 
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=True,
+        freeze_till=None,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts["UPDATED_BASE_MODEL_NAME"]
+    )
+
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    full_model.save(update_base_model_path)
+    
 
 
 if __name__ == '__main__':
@@ -38,11 +59,13 @@ if __name__ == '__main__':
 
     args.add_argument("--config", "-c", default="config/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
+
     parsed_args = args.parse_args()
+
     try:
-        logging.info(">>>>> stage 02 started")
-        prepare_base_model(config_path = parsed_args.config, params_path = parsed_args.params)
-        logging.info("stage 02 completed! base model is created >>>>>")
+        logging.info(">>>>> stage two started")
+        prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info("stage two completed! base model is created >>>>>")
     except Exception as e:
         logging.exception(e)
         raise e
